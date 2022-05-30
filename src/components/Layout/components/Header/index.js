bf07d56a8b4e4f69b6f1c60d32b6384e6aeaebea
hoangak47 <57388240+hoangak47@users.noreply.github.com>
2022-05-30 22:27:17 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/asset/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -7,7 +11,18 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('logo')}></div>
+                <img src={images.logo} alt="logo" />
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm tài khoản và video" />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
